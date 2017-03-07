992a7e7108a9351c34d2915acb4f788a14bc1023
Yioks-Mac <392811726@qq.com>
2017-03-07 19:09:45 +0800

    format
    format

--- Format.pl
+++ Format.pl
@@ -8,4 +8,42 @@ second: ^<<<<<
 third: ^<<<<  # 左边对其，字符长度为5，taobao最后一个o被截断
 	$text
 .
-write
+write;
+
+
+format EMPLOYEE = 
+=================================
+@<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< @<<
+$name $age
+@#####.##
+$salary
+================================
+.
+
+select(STDOUT);
+$~ = EMPLOYEE;
+@n = ("Ali", "Runoob", "Jaffer");
+@a = (20, 30, 40);
+@s = (2000.00, 2500.00, 4000.000);
+$i = 0;
+foreach (@n) {
+	$name = $_;
+	$age = $a[$i];
+	$salary = $s[$i++];
+	write;
+}
+
+$~ = "MYFORMAT";
+write;
+
+format MYFORMAT =
+=====================
+	Text #cabc a 
+====================
+.
+write;
+
+write;
+
+
+
